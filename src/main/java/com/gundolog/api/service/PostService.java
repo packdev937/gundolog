1d0c3a7e3c3f8a4d0b454e22451f4fe7381bd37b
gundorit <packdev937@gmail.com>
2023-09-16 23:30:56 +0900

    feat : 포스트 작성 로직 구현 write()

--- src/main/java/com/gundolog/api/service/PostService.java
+++ src/main/java/com/gundolog/api/service/PostService.java
@@ -1,13 +1,29 @@
 package com.gundolog.api.service;
 
+import com.gundolog.api.entity.Post;
+import com.gundolog.api.repository.PostRepository;
 import com.gundolog.api.request.PostCreate;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class PostService {
-    public void write(PostCreate postCreate){
 
+    private final PostRepository postRepository;
+
+    public void write(PostCreate postCreate) {
+        // Entity로 변환
+        Post newPost = Post.builder()
+            .title(postCreate.getTitle())
+            .content(postCreate.getContent())
+            .createdDate(LocalDateTime.now())
+            .updatedDate(LocalDateTime.now())
+            .build();
+
+        postRepository.save(newPost);
     }
 }
