01368bc29212e1330ef2271c7699ed31779f351e
gundorit <packdev937@gmail.com>
2023-09-16 23:16:04 +0900

    add : PostService.java

---
+++ src/main/java/com/gundolog/api/service/PostService.java
@@ -0,0 +1,13 @@
+package com.gundolog.api.service;
+
+import com.gundolog.api.request.PostCreate;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PostService {
+    public void write(PostCreate postCreate){
+
+    }
+}
