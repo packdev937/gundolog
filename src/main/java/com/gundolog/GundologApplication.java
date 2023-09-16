dc34d5c5ba9042b20a00007d85ed32c93afd9c1e
gundorit <packdev937@gmail.com>
2023-09-16 19:29:45 +0900

    feat : 시작 시 프로젝트 기본 값

---
+++ src/main/java/com/gundolog/GundologApplication.java
@@ -0,0 +1,13 @@
+package com.gundolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GundologApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GundologApplication.class, args);
+    }
+
+}
