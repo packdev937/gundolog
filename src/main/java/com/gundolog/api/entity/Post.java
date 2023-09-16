5aab87e328f9532e813c154d308828f358dd1d74
gundorit <packdev937@gmail.com>
2023-09-16 23:30:33 +0900

    feat : 기본 생성자, 생성자, Builder 추가

--- src/main/java/com/gundolog/api/entity/Post.java
+++ src/main/java/com/gundolog/api/entity/Post.java
@@ -3,11 +3,19 @@ package com.gundolog.api.entity;
 
 import java.time.LocalDateTime;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,6 +24,16 @@ public class Post {
     @Column(name = "updated_at")
     private LocalDateTime updatedDate;
     private String title;
+    @Lob // Database에서 Long Text 형태로 갖고 있도록
     private String content;
 
+    @Builder
+    public Post(Long id, LocalDateTime createdDate, LocalDateTime updatedDate, String title,
+        String content) {
+        this.id = id;
+        this.createdDate = createdDate;
+        this.updatedDate = updatedDate;
+        this.title = title;
+        this.content = content;
+    }
 }
