ab4fd14785c61aba0286e719c34418223ba6f463
gundorit <packdev937@gmail.com>
2023-09-16 23:17:01 +0900

    add : PostRepository.java

---
+++ src/main/java/com/gundolog/api/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.gundolog.api.repository;
+
+import com.gundolog.api.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
