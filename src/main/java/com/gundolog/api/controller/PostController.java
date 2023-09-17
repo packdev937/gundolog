cf311cf1db2a32131db29702362c8869fce7b72a
gundorit <packdev937@gmail.com>
2023-09-17 12:03:47 +0900

    feat : 글 조회를 위한 /posts/{postId} 매핑 추가 및 메서드 추가

--- src/main/java/com/gundolog/api/controller/PostController.java
+++ src/main/java/com/gundolog/api/controller/PostController.java
@@ -1,21 +1,32 @@
 package com.gundolog.api.controller;
 
+import com.gundolog.api.entity.Post;
 import com.gundolog.api.request.PostCreate;
+import com.gundolog.api.service.PostService;
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class PostController {
 
+    private final PostService postService;
+
     @PostMapping("/posts")
-    public String post(@RequestBody @Valid PostCreate params) {
-        log.info("title : {}, content: {}", params.title, params.content);
-        return "Hello World";
+    public void post(@RequestBody @Valid PostCreate postCreate) {
+        postService.write(postCreate);
+        return;
+    }
+
+    @GetMapping("/posts/{postId}")
+    public Post get(@PathVariable(name = "postId") Long id) {
+        return postService.get(id);
     }
 }
-
-// ModelAttribute와 RequestBody의 차이는?
