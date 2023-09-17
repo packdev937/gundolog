49984d724f11c4e3179e7bd500ab96d7dc75c0eb
gundorit <packdev937@gmail.com>
2023-09-18 01:31:49 +0900

    feat : getList() 구현
    stream()을 이용해 Post -> PostResponse 반환

--- src/main/java/com/gundolog/api/service/PostService.java
+++ src/main/java/com/gundolog/api/service/PostService.java
@@ -5,7 +5,8 @@ import com.gundolog.api.repository.PostRepository;
 import com.gundolog.api.request.PostCreate;
 import com.gundolog.api.response.PostResponse;
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -34,13 +35,18 @@ public class PostService {
             () -> new IllegalArgumentException("존재하지 않는 글입니다")
         );
 
-        PostResponse response = PostResponse.builder()
+        return PostResponse.builder()
             .title(post.getTitle())
             .content(post.getContent())
             .id(post.getId())
             .build();
-
-        return response;
     }
 
+    public List<PostResponse> getList() {
+        return postRepository.findAll().stream()
+            .map(post -> PostResponse.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .content(post.getContent()).build()).collect(Collectors.toList());
+    }
 }
