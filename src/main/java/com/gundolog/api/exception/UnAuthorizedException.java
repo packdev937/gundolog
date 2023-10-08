5e57e30b62d8aaf1a1b6a8c3b39609701f9307d7
gundorit <packdev937@gmail.com>
2023-10-09 00:32:33 +0900

    feat : add UnAuthorizedException

---
+++ src/main/java/com/gundolog/api/exception/UnAuthorizedException.java
@@ -0,0 +1,17 @@
+package com.gundolog.api.exception;
+
+public class UnAuthorizedException extends GundologException {
+
+    public UnAuthorizedException(String message) {
+        super(message);
+    }
+
+    public UnAuthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    @Override
+    public int statusCode() {
+        return 404;
+    }
+}
