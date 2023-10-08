4cfb0fed92b410c89d2925b49c8c5894b052c3fc
gundorit <packdev937@gmail.com>
2023-10-09 00:32:42 +0900

    feat : add WebMvcConfig

---
+++ src/main/java/com/gundolog/api/config/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.gundolog.api.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+            .excludePathPatterns("/error");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthResolver());
+    }
+}
