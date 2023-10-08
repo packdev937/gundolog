de4b752c8905ebdafc58e4a54862baecb59ae7c5
gundorit <packdev937@gmail.com>
2023-10-09 00:32:09 +0900

    feat : add AuthInterceptor

---
+++ src/main/java/com/gundolog/api/config/AuthInterceptor.java
@@ -0,0 +1,32 @@
+package com.gundolog.api.config;
+
+import com.gundolog.api.exception.UnAuthorizedException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+        String accessToken = request.getParameter("accessToken");
+        if (accessToken != null && accessToken.equals("gundolog")) {
+            return true;
+        }
+        throw new UnAuthorizedException("권한이 없습니다.");
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+        ModelAndView modelAndView) throws Exception {
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
+        Object handler, Exception ex) throws Exception {
+        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+    }
+}
