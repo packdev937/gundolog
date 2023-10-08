538c7c6e4fc0f130ca3bb3e0a538bfaa3fd75144
gundorit <packdev937@gmail.com>
2023-10-09 08:36:01 +0900

    add : User.class

---
+++ src/main/java/com/gundolog/api/entity/User.java
@@ -0,0 +1,23 @@
+package com.gundolog.api.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String password;
+    private LocalDateTime createdAt;
+}
