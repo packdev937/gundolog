6730da4b372e8b53d16b40dfd516b1e831153727
gundorit <packdev937@gmail.com>
2023-10-09 08:22:06 +0900

    feat : add LoginDto

---
+++ src/main/java/com/gundolog/api/request/Login.java
@@ -0,0 +1,16 @@
+package com.gundolog.api.request;
+
+import javax.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
+public class Login {
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
+    private String password;
+}
