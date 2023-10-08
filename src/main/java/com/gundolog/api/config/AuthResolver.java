03e6abaa54caf67297a90f78eda91db7c235dea1
gundorit <packdev937@gmail.com>
2023-10-09 00:32:17 +0900

    feat : add AuthResolver

---
+++ src/main/java/com/gundolog/api/config/AuthResolver.java
@@ -0,0 +1,34 @@
+package com.gundolog.api.config;
+
+import com.gundolog.api.config.data.UserSession;
+import com.gundolog.api.exception.UnAuthorizedException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Slf4j
+public class AuthResolver implements HandlerMethodArgumentResolver {
+
+    // 넘어온 parameter가 UserSession이 맞아?
+    // DTO가 반드시 인증이 필요한 라우터다 ->
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.getParameterType().equals(UserSession.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+        NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        log.info(">>> resolveArgument()");
+        String accessToken = webRequest.getParameter("accessToken");
+        if (accessToken == null && !accessToken.equals("gundolog")) {
+            throw new UnAuthorizedException("권한이 없습니다.");
+        }
+        UserSession userSession = new UserSession();
+        userSession.name = "gundolog";
+        return userSession;
+    }
+}
