83c9a365bd780fb323a112fffc5798d3c58c74f9
gundorit <packdev937@gmail.com>
2023-10-09 13:08:30 +0900

    feat : findByAccessToken() 메소드 추가

--- src/main/java/com/gundolog/api/repository/SessionRepository.java
+++ src/main/java/com/gundolog/api/repository/SessionRepository.java
@@ -1,8 +1,10 @@
 package com.gundolog.api.repository;
 
 import com.gundolog.api.entity.Session;
+import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SessionRepository extends CrudRepository<Session, Long> {
 
+    Optional<Session> findByAccessToken(String accessToken);
 }
