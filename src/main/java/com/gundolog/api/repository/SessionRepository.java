bfc8c1b6df070be4cc8baf14963f0ce35f07b198
gundorit <packdev937@gmail.com>
2023-10-09 11:47:50 +0900

    feat : SessionRepository 생성

---
+++ src/main/java/com/gundolog/api/repository/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.gundolog.api.repository;
+
+import com.gundolog.api.entity.Session;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SessionRepository extends CrudRepository<Session, Long> {
+
+}
