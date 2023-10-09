52456a73347fbd9ed507b7f51af9fbd26fc325ee
gundorit <packdev937@gmail.com>
2023-10-09 11:47:04 +0900

    feat : User <-> Session 연관 관계 설정

--- src/main/java/com/gundolog/api/entity/Session.java
+++ src/main/java/com/gundolog/api/entity/Session.java
@@ -1,5 +1,33 @@
 package com.gundolog.api.entity;
 
+import java.util.UUID;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Session {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String accessToken;
+
+    @ManyToOne()
+    private User user;
+
+    @Builder
+    public Session(User user) {
+        this.accessToken = UUID.randomUUID().toString();
+        this.user = user;
+    }
 }
--- src/main/java/com/gundolog/api/entity/User.java
+++ src/main/java/com/gundolog/api/entity/User.java
@@ -1,11 +1,16 @@
 package com.gundolog.api.entity;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -20,4 +25,24 @@ public class User {
     private String email;
     private String password;
     private LocalDateTime createdAt;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Session> sessions = new ArrayList<>();
+
+    public Session addSession() {
+        Session session = Session.builder()
+            .user(this)
+            .build();
+        sessions.add(session);
+
+        return session;
+    }
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.createdAt = LocalDateTime.now();
+    }
 }
